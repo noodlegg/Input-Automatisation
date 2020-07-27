d6a325ba69958ff3f117d9ebc8c9d96008711f9d
NoodleGG <TGian98@gmail.com>
2020-07-28 00:59:10 +0200

    Process for Discord bot automation
    - Changed exception logs to specify the object name for easier debugging
    - Added a relative dragging function to class Objective
    - main.py contains the automation for the Discord bot interaction

--- main.py
+++ main.py
@@ -1,4 +1,83 @@
 from objective import Objective
+from tkinter import Tk
+import time
+import re
 
-TaskbarExpand = Objective()
-TaskbarExpand.clickAtImg('taskbar_expand.PNG')
+# Automated sequence of actions for interaction with Discord bot
+# Requires you to have the Discord client in the foreground
+# TODO: Instead of time.sleep() it should await for some period of time
+
+r = Tk()
+Discord = Objective("Discord")
+OCR = Objective("OCR")
+countSpawned = 0
+countClaimed = 0
+
+# Execution loop
+if __name__ == "__main__":
+    while True:
+        # Type something in Discord chat
+        Discord.clickAtImg("dc_input.PNG")
+        Discord.write(".")
+        Discord.hotkey("enter")
+        time.sleep(1)
+
+        # Check whether card spawned
+        try:
+            startCoord = Discord.locateImg("dc_spawn.PNG")
+            countSpawned += 1
+        except:
+            print("No card spawn detected!")
+            time.sleep(60)
+            continue
+            
+        # Click on the card to enlarge
+        Discord.clickAtCoord((startCoord[0]+100, startCoord[1]+100))
+        time.sleep(1)
+        try:
+            startCoord = Discord.locateImg("dc_card.PNG")
+        except:
+            print("Did not detect enlarged card!")
+            Discord.hotkey("esc")
+            time.sleep(2)
+            continue
+
+        # Screenshot the code using OCR
+        OCR.hotkey("ctrl", "d", "1")
+        # Offset to start at the top left of the code
+        coords = list(startCoord)
+        coords[0] += 7
+        coords[1] -= 64
+        startCoord = tuple(coords)
+        OCR.dragRelative(startCoord, 295, 40)
+
+        # Close card enlargement in Discord
+        Discord.clickAtCoord(startCoord)
+        Discord.hotkey("esc")
+        time.sleep(0.5)
+
+        # Retrieve code from OCR
+        try:
+            OCR.clickAtImg("ocr_read.PNG")
+            time.sleep(1.5)
+            OCR.clickAtImg("ocr_copy.PNG")
+        except:
+            print("Failed to click on OCR interface!")
+            continue
+        code = r.clipboard_get()
+        # Only keep letters without spaces using RegEx
+        code = re.sub('[^a-zA-Z]+', '', code)
+        output = "claim " + code
+        print("Output is going to be: " + output)
+        Discord.clickAtImg("dc_input.PNG")
+        Discord.write(output)
+        Discord.hotkey("enter")
+
+        # Check whether card is claimed successfully
+        try:
+            Discord.locateImg("dc_claimed.PNG")
+            countClaimed += 1
+        except:
+            print("Failed to claim the card!")
+        print("Spawns: " + str(countSpawned) + " / Claimed: " + str(countClaimed))
+        time.sleep(120)
--- objective.py
+++ objective.py
@@ -7,35 +7,41 @@ import cv2
 
 class Objective(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, name="Objective"):
+        self._name = name
+
+    def name(self):
+        return self._name
 
     def locateImg(self, img):
         path = r'./images/' + img
 
         # Pre-condition: path to img file must exist
         if not os.path.exists(path):
-            raise Exception("Objective.locateImg() pre-condition failed, " + path + " does not exist!")
+            raise Exception(self._name + ".locateImg() pre-condition failed, " + path + " does not exist!")
 
         # Pre-condition: file must be of valid img type
         if not (imghdr.what(path) == 'png') and not (imghdr.what(path) == 'jpeg'):
-            raise Exception("Objective.locateImg() pre-condition failed, " + path + " is not of type jpeg nor png")
+            raise Exception(self._name + ".locateImg() pre-condition failed, " + path + " is not of type jpeg nor png")
         
         image = cv2.imread(path)
 
         # Post-condition: returns 4-integer tuple (left, top, width, height)
-        return pyautogui.locateOnScreen(image, confidence=0.9)
+        coordinates = pyautogui.locateOnScreen(image, confidence=0.8)
+        if coordinates == None:
+            raise Exception(self._name + "./locateImg() failed, could not detect " + path + " on screen")
+
+        return coordinates
 
     def clickAtCoord(self, coordinates):
-
         # Pre-condition: coordinates must be given
         if (coordinates == None):
-            raise Exception("Objective.clickAt() pre-condition failed, coordinates == None")
+            raise Exception(self._name + ".clickAt() pre-condition failed, coordinates == None")
 
         # Pre-condition: coordinates must be within a valid range
         screenSize = pyautogui.size()
         if (coordinates[0] > screenSize[0]) or (coordinates[1] > screenSize[1]):
-            raise Exception("Objective.clickAt() pre-condition failed, cannot reach " + coordinates[slice(2)] + " on " + screenSize)
+            raise Exception(self._name + ".clickAt() pre-condition failed, cannot reach " + coordinates[slice(2)] + " on " + screenSize)
 
         pyautogui.click(x=coordinates[0], y=coordinates[1])
 
@@ -43,22 +49,36 @@ class Objective(object):
         self.clickAtCoord(pyautogui.center(self.locateImg(img)))
 
     def hotkey(self, *arg):
-
         # Pre-condition: at least one argument passed
         if not len(arg) > 0:
-            raise Exception("Objective.hotkey() pre-condition failed, " + len(arg) + " arguments passed!")
+            raise Exception(self._name + ".hotkey() pre-condition failed, " + len(arg) + " arguments passed!")
 
         # Pre-condition: arguments must be of type string
         for argument in arg:
             if not isinstance(argument, str):
-                raise Exception("Objective.hotkey() pre-condition failed, " + argument + " is not of type string!")
+                raise Exception(self._name + ".hotkey() pre-condition failed, " + argument + " is not of type string!")
 
         pyautogui.hotkey(*arg)
 
     def write(self, input):
-
         # Pre-condition: input must be of type string
         if not isinstance(input, str):
-            raise Exception("Objective.write() pre-condition failed, " + input + " is not of type string!")
+            raise Exception(self._name + ".write() pre-condition failed, " + input + " is not of type string!")
 
         pyautogui.write(input)
+
+    def dragRelative(self, startCoord, xDist, yDist):
+        # TODO: refactor code for DRY principle, here are duplicate pre-conditions of function clickAtCoord()
+        # Pre-condition: coordinates must be given
+        if (startCoord == None):
+            raise Exception(self._name + ".clickAt() pre-condition failed, startCoord == None")
+
+        # Pre-condition: coordinates must be within a valid range
+        screenSize = pyautogui.size()
+        if (startCoord[0] > screenSize[0]) or (startCoord[1] > screenSize[1]):
+            raise Exception(self._name + ".clickAt() pre-condition failed, cannot reach " + startCoord[slice(2)] + " on " + screenSize)
+
+        pyautogui.moveTo(x=startCoord[0], y=startCoord[1])
+        pyautogui.drag(xDist, yDist, 0.3, button='left')
+
+
